e3ba2bc531e551f56ac929652f395481d8100b51
James Cooper-Stanbury <jpcs@freelancer.com>
2015-05-26 23:20:53 +1000

    refactored api to set up for a command based infrastructure

--- api.py
+++ api.py
@@ -1,21 +1,30 @@
 from lights import DaftPunk
-from flask import Flask
-import json
+from flask import Flask, jsonify, request
+
+from argparse import ArgumentParser
 
 app = Flask(__name__)
-
+parser = ArgumentParser()
 
 @app.route('/')
 def hello_world():
     return 'Welcome to Daft Punk!'
 
 
-@app.route('/colour/<l>/<c>')
-def change_colour(l, c):
-    x = app.d.colour(l, str(c.strip()))
-    return json.dumps(x)
+@app.route('/<command>')
+def command(command):
+    if command == "colour":
+        res = app.d.colour(request.args.get('light'), str(request.args.get('colour')))
+    # elif command == "on":
+    #     res = app.d.on(request.args.get('light'), bool(request.args.get('on')))
+
+    return jsonify(res)
 
 
 if __name__ == '__main__':
-    app.d = DaftPunk("sydney.json")
-    app.run(debug=True)
+    parser.add_argument("-c", "--config", dest="config_file", help="which config file to use")
+    args = parser.parse_args()
+
+    app.d = DaftPunk(args.config_file)
+
+    app.run(host="0.0.0.0", debug=True)
