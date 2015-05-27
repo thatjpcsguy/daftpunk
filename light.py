c62a55ffcf88c935a11f1cf93d283e831756b207
James Cooper-Stanbury <jpcs@freelancer.com>
2015-05-27 23:04:23 +1000

    add update method to a light, to bypass the beautifulhue library

--- light.py
+++ light.py
@@ -1,3 +1,7 @@
+import requests
+import json
+
+
 class Light():
     bridge = None
     light_id = None
@@ -5,3 +9,11 @@ class Light():
     def __init__(self, bridge, light_id):
         self.bridge = bridge
         self.light_id = light_id
+
+    def update(self, data):
+        path = '{prefix}/lights/{id}/state'.format(prefix=self.bridge.prefix, id=self.light_id)
+
+        print path
+        print json.dumps(data)
+        res = requests.put(path, json.dumps(data))
+        return res.status_code, json.loads(res.text)
