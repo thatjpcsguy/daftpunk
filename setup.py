aae8a0684cbdc010ea01e780ef47ebf1262f8dd8
James Peter Cooper-Stanbury <jpcs@freelancer.com>
2015-06-12 19:53:15 +1000

    made python package

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='DaftPunk',
+    version='1.0',
+    description='Phillips Hue SDK',
+    author='James Peter Cooper-Stanbury',
+    author_email='james@cooperstanbury.com',
+    url='https://github.com/thatjpcsguy/daftpunk',
+    packages=['daftpunk']
+)
